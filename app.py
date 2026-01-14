de3d2c267d21740734c6c2579666cded3cc0b0b6
Gabrielsvdata <gabrielsilvanovieira7@gmail.com>
2026-01-13 21:23:34 -0300

    Fix gunicorn app instance

--- app.py
+++ app.py
@@ -34,6 +34,9 @@ def create_app(config_name=None):
     return app
 
 
+# Instância para Gunicorn (produção)
+app = create_app()
+
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=5000, debug=True)
